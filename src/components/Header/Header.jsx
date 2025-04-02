320ffe7759784fc3411fd55319fd4985b6361345
vardhanash <ashok.vardhan.a@gmail.com>
2025-04-03 03:24:07 +0530

    register button to redirect to test page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import ReusableButton from "../../components/ReusableButton/ReusableButton";
 import logo from "/assets/logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
+
 const Header = () => {
+
+  const navigate = useNavigate();
+  const handleMobileMenu = () => {
+    console.log("clicked test")
+    navigate("/test", { state: { } });
+  };
+
+  
+
   return (
     <div className="header-container py-2 px-4 shadow-sm bg-white">
       <nav className="navbar d-flex justify-content-between align-items-center">
@@ -12,7 +24,7 @@ const Header = () => {
         </a>
         <div className="d-flex gap-2 flex-nowrap">
           <ReusableButton className="reusable-btn login btn-sm" label="Login" onClick={() => console.log("Login clicked")} />
-          <ReusableButton className="reusable-btn register btn-sm" label="Register" onClick={() => console.log("Register clicked")} />
+          <ReusableButton className="reusable-btn register btn-sm" label="Register" onClick={() => handleMobileMenu()} />
         </div>
       </nav>
     </div>
